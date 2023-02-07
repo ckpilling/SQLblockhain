1a191147353a07446213eafdf68641c1c1abb730
PillingCK <PillingCK@G02BEXN00274>
2023-02-07 08:15:15 +0000

    Add project files.

---
+++ Pages/Clients/Edit.cshtml.cs
@@ -0,0 +1,117 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyStore.Pages.Clients;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace MyStore.Pages.Clients
+{
+    public class EditModel : PageModel
+    {
+
+        public ClientInfo clientInfo = new ClientInfo();
+        public string errorMessage = "";
+        public string successMessage = "";
+
+        public void OnGet()
+        {
+
+            String id = Request.Query["id"];
+
+            try
+            {
+                String connectionString = "Data Source=192.168.0.28\\;Initial Catalog=clients;Persist Security Info=True;User ID=sa;Password=password0. ";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+
+                    connection.Open();
+                    String sql = "SELECT * FROM clients where id=@id";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                //ClientInfo clientInfo = new ClientInfo();
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+                                clientInfo.created_at = reader.GetDateTime(5).ToString();
+
+
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                errorMessage = ex.Message;
+            }
+
+        }
+
+
+        public void OnPost()
+        {
+
+            clientInfo.id = Request.Form["id"];
+            clientInfo.name = Request.Form["name"];
+            clientInfo.email = Request.Form["email"];
+            clientInfo.phone = Request.Form["phone"];
+            clientInfo.address = Request.Form["address"];
+
+
+            if (clientInfo.id.Length == 0 || clientInfo.name.Length == 0 || clientInfo.email.Length == 0 ||
+                clientInfo.phone.Length == 0 || clientInfo.address.Length == 0)
+            {
+                errorMessage = "All fields must be completed";
+                return;
+
+            }
+
+
+            try
+            {
+
+                String connectionString = "Data Source=192.168.0.28\\;Initial Catalog=clients;Persist Security Info=True;User ID=sa;Password=password0. ";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "UPDATE clients " +
+                        "SET name=@name, email=@email, phone=@phone, address=@address " +
+                        "WHERE id=@id";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", clientInfo.name);
+                        command.Parameters.AddWithValue("@email", clientInfo.email);
+                        command.Parameters.AddWithValue("@address", clientInfo.address);
+                        command.Parameters.AddWithValue("@phone", clientInfo.phone);
+                        command.Parameters.AddWithValue("@id", clientInfo.id);
+
+                        command.ExecuteNonQuery();
+
+
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                errorMessage = ex.Message;
+                return;
+            }
+
+
+            Response.Redirect("/Clients/Index");
+        }
+    }
+}
